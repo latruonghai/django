642e9339ace876ae1b7cd9da61b822d5eacce535
La Truong Hai <latruonghai@gmail.com>
2021-05-17 15:04:17 +0700

    update 17/5/2021

--- task/views.py
+++ task/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import ToDoList, Item
 
 from django.http import HttpResponse
 
-def index(response):
-    return HttpResponse("<h1> Hello </h1>")
+def index(response, name):
+    t = ToDoList(name = "User {}".format(name))
+    t.save()
+    return HttpResponse("<h1>{} </h1>".format(ToDoList.objects.get(name = "User {}".format(name))))
 
 def v1(response):
     return HttpResponse("<h1> Hello Hai</h1>")
-# Create your views here.
+
+
